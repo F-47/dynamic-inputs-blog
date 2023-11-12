eb90ca683bc9d81657ee24d0a84b3497d2b4d7cc
Executioner47 <faresgalal09@gmail.com>
2023-11-12 04:31:58 +0200

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,222 @@
-import Image from 'next/image'
+"use client";
+import { z } from "zod";
+import { useForm, useFieldArray } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import Dropzone from "react-dropzone";
+import { cn } from "@/lib/utils";
+
+const validationSchema = z.object({
+  name: z.string().min(1, {
+    message: "Name is required",
+  }),
+  email: z.string().min(1, { message: "Email is required" }).email({
+    message: "Must be a valid email",
+  }),
+  products: z
+    .array(
+      z.object({
+        file: z.any(),
+        name: z.string().min(1, { message: "Product Name is required" }),
+        description: z.string().min(1, {
+          message: "Product Description is required",
+        }),
+        price: z.coerce.number(),
+      })
+    )
+    .nonempty({ message: "Product is required" }),
+});
+
+type FormValues = z.infer<typeof validationSchema>;
 
 export default function Home() {
+  const form = useForm<FormValues>({
+    resolver: zodResolver(validationSchema),
+    mode: "onBlur",
+    defaultValues: {
+      name: "",
+      email: "",
+      products: [],
+    },
+  });
+
+  const { fields, append } = useFieldArray({
+    name: "products",
+    control: form.control,
+  });
+
+  console.log(form.formState.errors);
+
+  const onSubmit = (values: FormValues) => {
+    console.log(values);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className="flex justify-center items-center h-screen">
+      <Form {...form}>
+        <form
+          action=""
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex-1 max-w-md space-y-5"
+        >
+          <div className="relative name">
+            <FormField
+              name="name"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-base">Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Enter your name"
+                      type="text"
+                      className="mt-3"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage className="text-red-500 capitalize" />
+                </FormItem>
+              )}
             />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+          </div>
+          <div className="relative email">
+            <FormField
+              name="email"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-base">Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Enter your email"
+                      type="email"
+                      className="mt-3"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage className="text-red-500 capitalize" />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="relative products_name_price_desc">
+            {fields.map((_, index) => {
+              return (
+                <div key={index}>
+                  <div className="mt-7 mb-2 text-xl font-bold">
+                    {form.getValues(`products.${index}.file.name`)}
+                  </div>
+                  <div className="flex gap-x-3">
+                    <FormField
+                      control={form.control}
+                      key={index}
+                      name={`products.${index}.name`}
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Product Name</FormLabel>
+                          <FormControl>
+                            <Input {...field} />
+                          </FormControl>
+                          <FormMessage className="text-red-500 capitalize" />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      key={index + 1}
+                      name={`products.${index}.description`}
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Product Description</FormLabel>
+                          <FormControl>
+                            <Input {...field} />
+                          </FormControl>
+                          <FormMessage className="text-red-500 capitalize" />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      key={index + 2}
+                      name={`products.${index}.price`}
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Product Price</FormLabel>
+                          <FormControl>
+                            <Input {...field} />
+                          </FormControl>
+                          <FormMessage className="text-red-500 capitalize" />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          <div className="relative products">
+            <FormField
+              control={form.control}
+              name="products"
+              render={() => (
+                <Dropzone
+                  accept={{
+                    "image/*": [".jpg", ".jpeg", ".png"],
+                  }}
+                  onDropAccepted={(acceptedFiles) => {
+                    acceptedFiles.map((acceptedFile) => {
+                      return append({
+                        file: acceptedFile,
+                        name: "",
+                        description: "",
+                        price: 0,
+                      });
+                    });
+                  }}
+                  multiple={true}
+                  maxSize={5000000}
+                >
+                  {({ getRootProps, getInputProps }) => (
+                    <div
+                      {...getRootProps({
+                        className: cn(
+                          "p-3 mb-4 flex flex-col items-center justify-center w-full rounded-md cursor-pointer border border-[#e2e8f0]"
+                        ),
+                      })}
+                    >
+                      <div className="flex items-center gap-x-3 mt-2 mb-2">
+                        <label
+                          htmlFor="Products"
+                          className={`text-sm text-[7E8DA0] cursor-pointer focus:outline-none focus:underline ${
+                            form.formState.errors.products && "text-red-500"
+                          }`}
+                          tabIndex={0}
+                        >
+                          Add your Product Images
+                          <input {...getInputProps()} />
+                        </label>
+                      </div>
+                    </div>
+                  )}
+                </Dropzone>
+              )}
+            />
+          </div>
+          <Button type="submit" className="!mt-0 w-full">
+            Add
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
 }
